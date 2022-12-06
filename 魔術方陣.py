569d1e35c75d627f005cb58cf8a09a965848fa0a
CSUMII\csumii <csumii1010@gmail.com>
2022-12-07 00:30:09 +0800

    update

--- 魔術方陣.py
+++ 魔術方陣.py
@@ -9,8 +9,8 @@ x, y = 0, N//2 #(i,j=N//2取整數)
 while f <= N**2: #N**2=平方
     num[x, y] = f
     f += 1
-    nx, ny = (x-1) % N, (y+1)% N
-    if num[nx, ny]: #新的
+    nx, ny = (x-1) % N, (y+1)% N #new x & new y
+    if num[nx, ny]: 
         x += 1
     else:
         x, y = nx, ny
