02e40b6e9475def48e53d38147f32292217d0c9e
CSUMII\csumii <csumii1010@gmail.com>
2023-01-30 00:00:13 +0800

    check1

--- hw.py
+++ hw.py
@@ -1,5 +1,5 @@
-number_d= {'0':'零','1':'壹','2':'貳','3':'參','4':'肆','5':'伍','6':'陸','7':'柒','8':'捌','9':'玖'}
-unit_list= {"","十","佰","仟","萬","十萬","千萬","億"}
+number_d = {'0':'零','1':'壹','2':'貳','3':'參','4':'肆','5':'伍','6':'陸','7':'柒','8':'捌','9':'玖'}
+unit_list = ["","十","佰","仟","萬","十萬","仟萬","億"]
 
 def check(number):
     if not isinstance(number,int):
@@ -13,8 +13,8 @@ def check(number):
 def toch(number):
     if not check(number):
         return ""
-    temp_ch = derect_translate(number)
-    upch = update(temp_ch)
+    ch = derect_translate(number)
+    upch = update(ch)
     if number >= 0:
         return upch
     return number_d[str(number)[0]] + upch
@@ -22,20 +22,20 @@ def toch(number):
 def derect_translate(number): 
     return [number_d[x] for x in str(abs(number))]
 
-def update(temp_ch):
-    temp_un = []
-    for ix, x in enumerate(temp_ch[::-1]):
+def update(ch):
+    un = []
+    for ix, x in enumerate(ch[::-1]):
         if x == "零":
-            if temp_un and (temp_un[-1] == "零" or temp_un[-1] == "零"):
+            if un and (un[-1] == "零" or un[-1] == "零"):
                 pass
-            elif temp_un or len(temp_ch) == 1:
-                temp_un.append(x)
+            elif un or len(ch) == 1:
+                un.append(x)
         else:
-            temp_un.append(x + unit_list(ix % 8))
-        if ix == 7 and len(temp_ch) > 8:
-            temp_un.append("億")
-    temp_un.reverse()
-    return "".join(temp_un)
+            un.append(x + unit_list[ix % 9])
+        if ix == 7 and len(ch) > 8:
+            un.append("億")
+    un.reverse()
+    return "".join(un)
   
 if __name__ == '__main__':
-   print(toch(51000))
+   print(toch(51235498)+'元整')
